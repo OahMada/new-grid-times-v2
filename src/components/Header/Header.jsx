605bb7d5482dbf5336037e0cb764f103911155dd
Adam Hao <haozg44@outlook.com>
2025-04-23 18:36:19 +0800

    finish the first exercise

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -9,62 +9,101 @@ import Logo from '../Logo';
 import Button from '../Button';
 
 const Header = () => {
-  return (
-    <header>
-      <SuperHeader>
-        <Row>
-          <ActionGroup>
-            <button>
-              <Search size={24} />
-            </button>
-            <button>
-              <Menu size={24} />
-            </button>
-          </ActionGroup>
-          <ActionGroup>
-            <button>
-              <User size={24} />
-            </button>
-          </ActionGroup>
-        </Row>
-      </SuperHeader>
-      <MainHeader>
-        <Logo />
-      </MainHeader>
-    </header>
-  );
+	return (
+		<header>
+			<SuperHeader>
+				<Row>
+					<ActionGroup>
+						<button>
+							<Search size={24} />
+						</button>
+						<button>
+							<Menu size={24} />
+						</button>
+					</ActionGroup>
+					<ActionGroup>
+						<button>
+							<User size={24} />
+						</button>
+					</ActionGroup>
+				</Row>
+			</SuperHeader>
+			<MainHeader>
+				<ActionGroup>
+					<button>
+						<Search size={24} />
+					</button>
+					<button>
+						<Menu size={24} />
+					</button>
+				</ActionGroup>
+				<Logo />
+				<SubscribeAction>
+					<SubscribeActionSpacer />
+					<Button>Subscribe</Button>
+					<SubscriberLink href='#'>Already a subscriber?</SubscriberLink>
+				</SubscribeAction>
+			</MainHeader>
+		</header>
+	);
 };
 
 const SuperHeader = styled.div`
-  padding: 16px 0;
-  background: var(--color-gray-900);
-  color: white;
+	padding: 16px 0;
+	background: var(--color-gray-900);
+	color: white;
+
+	@media ${QUERIES.desktopAndUp} {
+		display: none;
+	}
 `;
 
 const Row = styled(MaxWidthWrapper)`
-  display: flex;
-  justify-content: space-between;
+	display: flex;
+	justify-content: space-between;
 `;
 
 const ActionGroup = styled.div`
-  display: flex;
-  gap: 24px;
+	display: flex;
+	gap: 24px;
 
-  /*
+	/*
     FIX: Remove the inline spacing that comes with
     react-feather icons.
   */
-  svg {
-    display: block;
-  }
+	svg {
+		display: block;
+	}
 `;
 
 const MainHeader = styled(MaxWidthWrapper)`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  margin-top: 32px;
-  margin-bottom: 48px;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	margin-top: 32px;
+	margin-bottom: 48px;
+	height: ${98 / 16}rem;
+`;
+
+var SubscribeActionSpacer = styled.div`
+	flex: 1;
+`;
+
+var SubscribeAction = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	gap: 8px;
+	height: 100%;
+`;
+
+var SubscriberLink = styled.a`
+	text-decoration: underline;
+	font-size: ${14 / 16}rem;
+	font-style: italic;
+	font-weight: var(--font-weight-normal);
+	color: var(--color-gray-900);
+	flex: 1;
 `;
 
 export default Header;
--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -3,31 +3,29 @@ import styled from 'styled-components';
 import format from 'date-fns/format';
 
 const Logo = (props) => {
-  return (
-    <Wrapper>
-      <Link href="/" {...props}>
-        New Grid Times
-      </Link>
-      <TodaysDate>
-        {format(new Date(), 'EEEE, MMMM do, yyyy')}
-      </TodaysDate>
-    </Wrapper>
-  );
+	return (
+		<Wrapper>
+			<Link href='/' {...props}>
+				New Grid Times
+			</Link>
+			<TodaysDate>{format(new Date(), 'EEEE, MMMM do, yyyy')}</TodaysDate>
+		</Wrapper>
+	);
 };
 
 const Wrapper = styled.div`
-  text-align: center;
+	text-align: center;
 `;
 
 const Link = styled.a`
-  font-family: var(--font-family-logo);
-  font-size: 3rem;
+	font-family: var(--font-family-logo);
+	font-size: 3rem;
 `;
 
 const TodaysDate = styled.p`
-  font-size: 1.125rem;
-  font-weight: var(--font-weight-medium);
-  margin-top: -1rem;
+	font-size: 1.125rem;
+	font-weight: var(--font-weight-medium);
+	margin-top: -1rem;
 `;
 
 export default Logo;
